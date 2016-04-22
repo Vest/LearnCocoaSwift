5f1b0e0b4501a136776a5c6050d4804eabc33799
Vest <Vest@users.noreply.github.com>
2016-04-22 14:57:20 +0200

    ToDo: Finish the application Added outlets; Added actions; Implemented NSTableViewDataSource; Use an empty array as DS.

--- ToDo/ToDo/MainWindowController.swift
+++ ToDo/ToDo/MainWindowController.swift
@@ -8,13 +8,48 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSTableViewDataSource {
 
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var tableView: NSTableView!
+    
+    var items: [String] = []
+    
     override func windowDidLoad() {
         super.windowDidLoad()
     }
     
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+    
+    @IBAction func addToDo(sender: NSButton) {
+        let todo = textField.stringValue
+        
+        if todo.isEmpty {
+            textField.becomeFirstResponder()
+            
+            print("ToDo is empty")
+            return
+        }
+        textField.stringValue = ""
+        
+        print("Add todo: \(todo)")
+        
+        createNewItemWithName(todo)
+        
+        tableView.reloadData()
+    }
+    
+    func createNewItemWithName(item: String) {
+       items.append(item)
+    }
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        return items[row]
     }
 }
