7b7a5ce865c8116406128f802ad41c14a53aa79c
Vest <Vest@users.noreply.github.com>
2016-04-25 23:12:10 +0200

    KVC: uses nested properties;

--- kvc.playground/Contents.swift
+++ kvc.playground/Contents.swift
@@ -19,4 +19,19 @@ s.gradeLevel
 s.valueForKey("name")
 s.valueForKey("gradeLevel")
 
+class Person: NSObject {
+    var spouse: Person? = nil
+    var scooter: Scooter? = nil
+}
 
+class Scooter: NSObject {
+    var modelName: String? = nil
+}
+
+let scooter = Scooter()
+scooter.modelName = "BV250"
+let selectedPerson = Person()
+selectedPerson.spouse = Person()
+selectedPerson.spouse?.scooter = scooter
+
+let modelName = selectedPerson.valueForKeyPath("spouse.scooter.modelName") as! String
