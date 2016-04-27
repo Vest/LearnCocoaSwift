b91da29431226f75635a0406f21941dbae965199
Vest <Vest@users.noreply.github.com>
2016-04-27 13:23:55 +0200

    RaiseMan: added a new application Editing works; Sorting works;

---
+++ RaiseMan/RaiseMan/Document.swift
@@ -0,0 +1,45 @@
+//
+//  Document.swift
+//  RaiseMan
+//
+//  Created by Vest on 4/26/16.
+//  Copyright © 2016 Vest. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSDocument {
+
+    var employees: [Employee] = []
+    
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override var windowNibName: String? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return "Document"
+    }
+
+    override func dataOfType(typeName: String) throws -> NSData {
+        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
+        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+        return NSKeyedArchiver.archivedDataWithRootObject(employees)
+    }
+
+    override func readFromData(data: NSData, ofType typeName: String) throws {
+        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
+        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
+        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
+        print("read???")
+    }
+
+
+}
+
---
+++ RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Vest on 4/26/16.
+//  Copyright © 2016 Vest. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
